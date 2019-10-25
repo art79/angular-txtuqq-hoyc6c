8e8e7e9ed8935757ab1e697b9f01798262399b05
art79 <artm@cae.com>
2019-10-25 16:22:26 -0400

    inject CartService into product-details components

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+// Import the CartService from the cart.service.ts file.
+import { CartService} from '../cart.service';
 
 @Component({
   selector: 'app-cart',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
+  items;
 
-  constructor() { }
+  constructor(
+    // Inject the CartService to manage cart information.
+    private cartService: CartService
+  ) { }
 
   ngOnInit() {
+    // Set the items using the cart service's getItems() method. (You defined this method when you generated cart.service.ts.)
+    this.items = this.cartService.getItems()
   }
 
 }
